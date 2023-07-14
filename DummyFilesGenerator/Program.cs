ac7f20d1e2a10b0550e40f2a44dfb6e0120c4d4a
Odysseas95 <o.dy.se@hotmail.com>
2023-07-15 01:21:50 +0300

    upload

---
+++ DummyFilesGenerator/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Timers;
+
+namespace DummyFilesGenerator
+{
+    static class DummyFilesGenerator
+    {
+
+        public static Random Gen = new Random();
+        public static Random GenDirs = new Random();
+        public static Random Size = new Random();
+
+        public static System.Timers.Timer Tmr;
+
+        public static DateTime StartDate = new DateTime(2022, 1, 1);
+        public static int Range;
+        public static DateTime RndDate;
+        public static string SubsubFolder;
+        public static int SubDirs;
+
+        public static string Path = Properties.Settings.Default.Path;
+        public static string FileName;
+        public static int FilesNum = Properties.Settings.Default.FilesNum;
+        public static int DirNum = Properties.Settings.Default.DirNum;
+        public static int SizeMod = Properties.Settings.Default.SizeMod;
+        public static int IntervalMins = Properties.Settings.Default.IntervalMins;
+        public static string FileEx = Properties.Settings.Default.FileEx;
+        public static bool Flag = false;
+
+        static void Main()
+        {
+            try
+            {
+                
+                IntervalMins = IntervalMins * 60000;
+
+                Console.WriteLine("Timer started");
+                Tmr = new System.Timers.Timer();
+                Tmr.Interval = IntervalMins;
+                Tmr.Elapsed += OnTimedEvent;
+                Tmr.Enabled = true;
+
+                Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+                Console.ReadLine();
+                Console.WriteLine("Terminating the application...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            try
+            {
+                string path = Path;
+                Tmr.Enabled = false;
+                Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+                Console.WriteLine("Generating...");
+
+                using (var progress = new ProgressBar())
+                {
+                    for (int i = 0; i < DirNum; i++)
+                    {
+
+                        progress.Report((double)i / DirNum);
+                        Thread.Sleep(20);
+
+                        SubDirs = GenDirs.Next(DirNum);
+
+                        if (!Flag)
+                        {
+                            while (SubDirs <= DirNum)
+                            {
+                                SubsubFolder = new DirectoryInfo(path).CreateSubdirectory("subfolder" + SubDirs).FullName;
+
+                                using (var progress1 = new ProgressBar())
+                                {
+                                    for (int j = 0; j < FilesNum; j++)
+                                    {
+
+                                        progress1.Report((double)j / FilesNum);
+                                        Thread.Sleep(20);
+
+                                        RndDate = RandomDay();
+                                        Generate(SubsubFolder);
+
+                                    }
+                                }
+                                SubDirs++;
+                            }
+                            path = SubsubFolder;
+                        }
+                        else
+                        {
+                            using (var progress1 = new ProgressBar())
+                            {
+
+                                string[] subdirectoryEntries = Directory.GetDirectories(Path);
+
+                                foreach (string subdirectory in subdirectoryEntries)
+                                {
+                                    for (int j = 0; j < FilesNum; j++)
+                                    {
+
+                                        progress1.Report((double)j / FilesNum);
+                                        Thread.Sleep(20);
+
+                                        RndDate = RandomDay();
+                                        Generate(subdirectory);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.ToString() + " \nSettings values too high, getting default values");
+                FilesNum = 10;
+                DirNum = 5;
+                SizeMod = 500;
+                Console.WriteLine(ex.ToString() + $" \nFiles Number = {FilesNum} Directories Number = {DirNum} Size Modifier = {SizeMod}");
+
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.ToString() + " \nTHE APP WILL TERMINATE!");
+
+
+            }
+            catch (PathTooLongException ex)
+            {
+                Flag = true;
+                Console.WriteLine(ex.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+            finally
+            {
+                Console.WriteLine("DONE!");
+                Tmr.Enabled = true;
+            }
+        }
+        private static void Generate(string path)
+        {
+            string fileEx;
+            string[] extensionArray;
+            Random rnd = new Random();
+            if (!Flag)
+            {
+                extensionArray = FileEx.Split(' ');
+                int index = rnd.Next(extensionArray.Length);
+                fileEx = extensionArray[index];
+                FileName = System.IO.Path.ChangeExtension(System.IO.Path.GetRandomFileName(), fileEx);
+
+            }
+            else
+            {
+                FileName = System.IO.Path.ChangeExtension(System.IO.Path.GetRandomFileName(), ".log");
+            }
+            
+            path = System.IO.Path.Combine(path, FileName);
+
+            //Find another way to generate large files
+            var bytes = new byte[SizeMod * DirNum * FilesNum];
+            Size.NextBytes(bytes);
+
+            using (StreamWriter w = File.AppendText(path))
+            {
+                foreach (byte byteValue in bytes)
+                {
+                    w.WriteLine(byteValue);
+                }
+            }
+
+            File.SetLastWriteTime(path, RndDate);
+
+        }
+        private static DateTime RandomDay()
+        {
+            Range = (DateTime.Today - StartDate).Days;
+            return StartDate.AddDays(Gen.Next(Range));
+        }
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,52 @@
+# Dummy Files Generator
+
+This project is a dummy files generator that creates random files with configurable settings. It generates files with random names, sizes, and timestamps for testing or demonstration purposes.
+
+## Getting Started
+
+These instructions will help you get a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+- [.NET Framework](https://dotnet.microsoft.com/download/dotnet-framework) - Make sure you have .NET Framework installed on your machine.
+
+### Installation
+
+1. Clone the repository to your local machine using the following command:
+
+2. Open the project in your preferred Integrated Development Environment (IDE).
+
+3. Build the project to resolve any dependencies and compile the code.
+
+### Usage
+
+1. Modify the desired settings in the `app.config` file, including the path, file and directory numbers, size modifier, interval, and file extension.
+
+2. Run the project, and it will start generating dummy files based on the configured settings.
+
+3. The progress of file generation will be displayed in the console, and you can press `Enter` to terminate the application.
+
+### License
+
+This project is licensed under the [MIT License](LICENSE). Feel free to modify, distribute, and use the code according to the terms of the license.
+
+### Acknowledgments
+
+This project was inspired by the need for generating dummy files for testing purposes. Special thanks to the creators and contributors of the external libraries and resources used in this project.
+
+## Contributing
+
+Contributions to this project are welcome. If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## Authors
+
+- [Odysseas Athanasiadis](https://github.com/Odysseas95) - Initial implementation
+
+## Feedback and Support
+
+If you have any feedback, questions, or need support, please feel free to contact me or open an issue in the GitHub repository.
+
+## Resources
+
+- [Microsoft .NET Framework](https://dotnet.microsoft.com/)
+- [ProgressBar](https://github.com/Mpdreamz/shellprogressbar) - A library used for displaying progress bars in the console.
